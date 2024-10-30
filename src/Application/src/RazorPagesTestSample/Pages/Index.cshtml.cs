95228923d3fefd5b2f4c27228ae10fd0630305ff
Jonathan Solarz <jsolarz@microsoft.com>
2024-10-30 15:27:00 +0200

    changes

--- src/Application/src/RazorPagesTestSample/Pages/Index.cshtml.cs
+++ src/Application/src/RazorPagesTestSample/Pages/Index.cshtml.cs
@@ -93,9 +93,14 @@ namespace RazorPagesTestSample.Pages
         }
 
         public static void WriteToDirectory(ZipArchiveEntry entry, string destDirectory)
-        {
-            string destFileName = Path.Combine(destDirectory, entry.FullName);
-            entry.ExtractToFile(destFileName);
-        }
+		{
+			string destFileName = Path.GetFullPath(Path.Combine(destDirectory, entry.FullName));
+			string fullDestDirPath = Path.GetFullPath(destDirectory + Path.DirectorySeparatorChar);
+			if (!destFileName.StartsWith(fullDestDirPath)) {
+				throw new System.InvalidOperationException("Entry is outside the target dir: " + destFileName);
+			}
+			entry.ExtractToFile(destFileName);
+		}
+
     }
 }
